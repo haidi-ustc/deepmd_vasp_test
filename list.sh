93eba36d95ab2d84fe63367071fc38b0730b085a
AnguseZhang <529133328@qq.con>
2019-07-17 17:28:48 +0800

    add sigma

--- list.sh
+++ list.sh
@@ -2,6 +2,7 @@ list="Be Al Ti Cu Zn Ge Zr Nb Ag Sn Ta W Pt Au Mg P S C Si B"
 for i in $list
 do 
   #str="https://raw.githubusercontent.com/haidi-ustc/deepmd_vasp_test/master/encut/${i}_encut.png"
-  str="https://raw.githubusercontent.com/haidi-ustc/deepmd_vasp_test/master/kpoints/${i}_kpionts.png"
+  #str="https://raw.githubusercontent.com/haidi-ustc/deepmd_vasp_test/master/kpoints/${i}_kpoints.png"
+  str="https://raw.githubusercontent.com/haidi-ustc/deepmd_vasp_test/master/sigma/${i}_sigma.png"
   echo "![$i]($str)"
 done
